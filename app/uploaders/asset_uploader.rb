225e2c93a1edb7f1f7005fcf7e8fbcd2e84bb72c
Eduardo <eduardo.s.tanaka@gmail.com>
2015-07-26 05:35:30 -0300

    cloudinary

--- app/uploaders/asset_uploader.rb
+++ app/uploaders/asset_uploader.rb
@@ -9,14 +9,14 @@ class AssetUploader < CarrierWave::Uploader::Base
 
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  #storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  #def store_dir
+  #  "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  #end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
@@ -54,10 +54,10 @@ class AssetUploader < CarrierWave::Uploader::Base
   #version :thumb do
   #  process resize_to_fill: [200,200]
   #end
-  
+
 
   version :standard do
-    process :resize_to_fill => [600, 400, :north]
+    process :resize_to_fill => [100, 150, :north]
   end
 
   version :thumbnail do
